ed320a1ddccaa3dde956610e789ddf6bc2aa1b47
hyeongju <kokiguitars@gmail.com>
2021-05-16 13:13:45 +0900

    Add expo-secure-store to remember credentials during signin

--- reducers/userSlice.js
+++ reducers/userSlice.js
@@ -1,24 +1,63 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import * as SecureStore from 'expo-secure-store';
 import userAPI from '../api/user';
+
+
+const rememberCredentials = async (credentials) => {
+  try {
+    await SecureStore.setItemAsync(
+      'userCredentials',
+      JSON.stringify(credentials)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const readCredentials = async () => {
+  try {
+    const credentials = await SecureStore.getItemAsync('userCredentials');
+
+    if (credentials) {
+      const parsedCredentials = JSON.parse(credentials);
+      return parsedCredentials;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const clearCredentials = async () => {
+  try {
+    await SecureStore.deleteItemAsync('userCredentials');
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const fetchSignin = createAsyncThunk(
   'user/fetchSignin',
   async (loginInput, thunkAPI) => {
+    if (!loginInput) {
+      loginInput = await readCredentials();
+    }
+
     try {
       const response = await userAPI.requestSignin(loginInput);
-      const { appIdToken, currentUser } = response;
 
       if (response.code === 200) {
-        // await Keychain.setGenericPassword(
-        //   currentUser.email,
-        //   currentUser.password
-        // );
+        rememberCredentials({
+          email: loginInput.email,
+          password: loginInput.password
+        });
+
         return response.data;
       } else {
+        await clearCredentials();
         return thunkAPI.rejectWithValue(response);
       }
     } catch (err) {
-      throw err;
+      console.log("err", err);
     }
   }
 );
